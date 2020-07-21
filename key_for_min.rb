a72e96e8b47218b042b722b3b1641513d6c45f0e
David Felix <david.felix02@hotmail.com>
2020-07-21 20:10:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,11 @@
 
 def key_for_min_value(name_hash)
   count = 0
+<<<<<<< HEAD
   min_value = 0
   the_key = :symbol
+=======
+>>>>>>> 3b080d79ef1b6471a570c9119198b7371d0b7bac
   if name_hash.empty?
     return nil
   end
@@ -14,12 +17,20 @@ def key_for_min_value(name_hash)
     if value.is_a? Integer
       
       if count == 0
+<<<<<<< HEAD
         min_value = value
         count+=1
         the_key = key
       end
       
       if value < min_value
+=======
+        min_value = name_hash[key]
+        count+=1
+      end
+      
+      if name_hash[key] < min_value
+>>>>>>> 3b080d79ef1b6471a570c9119198b7371d0b7bac
         the_key = key
         min_value = name_hash[key]
       end
